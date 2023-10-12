2e2b14089a4153384b2c791b5c5ed3b6eb83f1f5
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2023-10-12 15:13:46 +0200

    .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase : {
+        apiKey: "AIzaSyAZsyHOjBAZsOL24oOGAnYK5nijzBHsL3k",
+        authDomain: "dedapp-ea169.firebaseapp.com",
+        projectId: "dedapp-ea169",
+        storageBucket: "dedapp-ea169.appspot.com",
+        messagingSenderId: "603538011285",
+        appId: "1:603538011285:web:d6fc6f5b2c1e6cda0b179c"
+    }
+  };
+  
